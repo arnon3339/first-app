628636adaecbaa464230ac876b7a119958b7d1bf
Arnon Songmoolnak <arnon3339@gmail.com>
2025-06-22 11:44:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("🎈 My new app1")
 st.write(
-    "Let's start building! For help and inspiration)."
+    "Let's start building! For help and inspiration 12342345)."
 )
