d489f785c98fb99323d3b71786b8480dc1e5ea60
Arnon Songmoolnak <arnon3339@gmail.com>
2025-07-20 11:47:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title("agent archives")
 name = st.text_input("enter your name")
 st.write(f"welcome {name}") 
 option = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
+    "select character?",
+    ("Lelouch", "Itachi Uchiha", "Sorawong", "Jesus", "mahatma Gandhi"),
 )
 
