a1b31ae92fe102c9460e26221d9d602010ece9ab
Arnon Songmoolnak <arnon3339@gmail.com>
2025-07-20 11:43:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,8 @@ import streamlit as st
 st.title("agent archives")
 name = st.text_input("enter your name")
 st.write(f"welcome {name}") 
-st.selectbox(["x", "y"])
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
+)
+
