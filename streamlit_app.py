4f1bc2ab59e356666acfba6c893699766c7cc163
Arnon Songmoolnak <arnon3339@gmail.com>
2025-07-27 11:52:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ done = {"Lelouch":"sacrificed himself for world peace",
               "Sorawong":"help the community and the sport industry bringing lots of events to Thailand",
         "Jesus":"healing the sick, feeding the hungry, and ultimately sacrificing himself for the salvation of humanity.", 
               "mahatma Gandhi":"advocacy of social justice and equality. His actions resulted in the end of British colonial rule in India and inspired movements for civil rights and freedom around the world. Creating a culture of peace, in proving the effectiveness of non-violent non-cooperation."}
+st.write("What they did for greater good.")
 st.write(done[option])
 Job = {"Lelouch":"leads a rebellion against the Holy Britannian Empire and becomes 99th emperor Britannian Empire.",
        "Itachi Uchiha":"Anbu captain and akaski member", 
